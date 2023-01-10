73badd72cad8f2fa9e3925e388365876680d19b7
AyalaGonzalo <SoloEstudioTrabajo@gmail.com>
2023-01-10 11:09:35 -0300

    some style css changed and adden an image

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 
-const skill = document.querySelector('[data-button]');
+const skill = document.querySelector('[data-button]')
 const list = document.querySelector('[data-list]')
 
 
-skill.addEventListener('click', () =>{
+skill.addEventListener('click', (e) =>{
 	
 	
-	console.log('you click me');
-	list.style.display = 'block';
-	skill.classList.remove('skill-h3')
-	skill.removeAttribute('data-button')
+	console.log(this,e.target)
+	listFun()
+
+	
 	
 })
+
+
+
+function listFun(){
+	
+	
+	list.classList.toggle('displayed')
+	list.classList.toggle('hidden')
+}
+
+
+
+
+
+/*
+function recFun(){
+	
+	
+	
+	
+	let width = 1;
+	let interval = setInterval(frame,10)
+	
+	function frame(){
+		
+		
+		if(width < 200){
+			
+			width++
+			rectangle.style.width = width + 'px';
+			
+		}else{
+			
+			clearInterval(interval)
+			
+		}
+		
+		
+		
+	}
+	
+	
+}
+
+*/
